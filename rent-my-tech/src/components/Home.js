9049c2fd69362f35d438161bf4634527eff65842
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-02-01 18:45:57 -0600

    hopefully this will make it so we can sync

--- rent-my-tech/src/components/Home.js
+++ rent-my-tech/src/components/Home.js
@@ -12,6 +12,7 @@ export default function Home(props) {
 
 
     return (
+      <div>
         <Navigation>
         <div className='red-box'></div>
          <h3>Rent My <span>Tech </span></h3> 
@@ -21,17 +22,34 @@ export default function Home(props) {
     <Link to='/login'>Login</Link>
     </div>
     </Navigation>
+<TopContainer>
+<LeftContainer>
+  
+  </LeftContainer>
+<RightContainer>
+<h3></h3>
+</RightContainer>
+
+</TopContainer>
+
+
+
+
+    </div>
         
     )
 }
 
 //STYLES----------------------------------
+
+
+//NAV-----------------------------------------------
 const Navigation = styled.div`
 box-sizing:border-box;
 background: #385A64;
 color: #f3e8e8;
 width:100vw;
-height: 10%;
+margin-top: -1rem;
 display:flex;
 justify-content:space-between;
 align-items:center;
@@ -85,7 +103,30 @@ a {
 a:hover {
   color:#df5f68;
 } 
+`
+// Top Container--------------------------------------------------------
+const TopContainer = styled.div`
+width: 100vw;
+height: 57.6rem;
+display:flex;
+`
+
+const LeftContainer = styled.div`
+background: url("https://i.ibb.co/d2HKgc8/Devices-rafiki.png");
 
 
+background-size: cover; 
+background-repeat:   no-repeat;
+background-position: center center;  
+flex:1;`
 
+const RightContainer = styled.div`
+box-sizing:border-box;
+/* border: 1px solid black; */
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+font-family: 'Poppins', sans-serif;
+flex:1;
+display:flex;
+flex-direction:column;
+align-items: center;
 `
